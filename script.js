f656fe0d18951ee58c72b5713893c956581ff815
Rouqaya Abdulhaq <ruqia334@gmail.com>
2018-10-09 11:43:18 +0300

    wrote the move functions and added the controls divs

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 let snake = document.querySelector("#snake");
 let playfield = document.querySelector("#playfield");
+let buttonUp = document.querySelector("up");
+let buttonLeft = document.querySelector("left");
+let buttonDown = document.querySelector("down");
+let buttonRight = document.querySelector("right");
+let controls = document.querySelectorAll("controls");
+let positionX = 0 ;
+let positionY = 0;
 
 const moveLeft =()=>{
-	snake.style.right += 10 + "px"; 
+	positionX-=10;
+	snake.style.left = positionX + "px"; 
+}
+const moveRight =()=> { 
+	positionX += 10 ;
+	snake.style.left = positionX +"px";
+}
+const moveUp =()=>{
+	positionY -= 10;
+	snake.style.top = positionY + "px"; 
+}
+const moveDown = ()=>{
+	positionY += 10;
+	snake.style.top = positionY + "px"; 
 }
 
-const moveRight =()=> {
-	snake.style.left += 10 + "px";
-}
+
+
+
 
 
