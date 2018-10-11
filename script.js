d5d926d3c9777571b739a2af5f2c6a1a0be9f968
Rouqaya Abdulhaq <ruqia334@gmail.com>
2018-10-11 12:37:29 +0300

    adjusted the controls

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let snake = document.querySelector("#snake");
 let playfield = document.querySelector("#playfield");
-let buttonUp = document.querySelector("up");
-let buttonLeft = document.querySelector("left");
-let buttonDown = document.querySelector("down");
-let buttonRight = document.querySelector("right");
-let controls = document.querySelectorAll("controls");
+let buttonUp = document.querySelector("#up");
+let buttonLeft = document.querySelector("#left");
+let buttonDown = document.querySelector("#down");
+let buttonRight = document.querySelector("#right");
+let controls = document.querySelectorAll(".controls");
 let positionX = 0 ;
 let positionY = 0;
 
@@ -24,6 +24,12 @@ const moveDown = ()=>{
 	positionY += 10;
 	snake.style.top = positionY + "px"; 
 }
+
+buttonUp.addEventListener("click",moveUp);
+buttonLeft.addEventListener("click",moveLeft);
+buttonDown.addEventListener("click",moveDown);
+buttonRight.addEventListener("click",moveRight);
+
 
 
 
