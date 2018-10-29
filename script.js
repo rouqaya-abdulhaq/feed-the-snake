fbf28713c0f93745b16d854b9caadabfaa8e2e9f
Rouqaya Abdulhaq <ruqia334@gmail.com>
2018-10-29 13:59:39 +0300

    created the first draft for the game play

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ let controls = document.querySelectorAll(".controls");
 let positionX = 0 ;
 let positionY = 0;
 let operators = ["+","-","*"];
-
+let gamePlay = document.querySelector("#game-play");
+let proplem;
+let input;
 
 const positionFood =()=>{
     foodPosX = Math.random()*playfield.offsetWidth;
@@ -41,15 +43,51 @@ const randomProplem =()=>{
 	let num1 = Math.floor(Math.random()*256);
     let num2 = Math.floor(Math.random()*256);
     let operator = operators[Math.floor(Math.random()*operators.length)];
-    let proplem = num1+operator+num2;
+    let proplem = num1+operator+num2+" =";
     return proplem; 
 }
+//returns a  simple random mathmatical proplem in string format 
+const question = ()=>{
+	let mathQuestion = document.createElement("p"); 
+	mathQuestion.appendChild(document.createTextNode(randomProplem()));
+	mathQuestion.id = "question";
+	return mathQuestion;
+}
+//returns an input box for the user input
+const createAnswerInput =()=>{
+	let userAnswer = document.createElement("input");
+	userAnswer.id = "answer";
+	return userAnswer;
+}
+//this method is not working properly
+//it should get the user answer and check if it is correct
+const validateAnswer =(event)=>{
+    if (input.value.length > 0 && event.keyCode === 13) {
+		return input.value === eval(proplem);
+	}
+}
+//this method should remove an existing question and input box
+//in case of correct answer or the user pressed the controls again
+const removeQuestion =(proplem,input)=>{
+    gamePlay.removeChild(proplem);
+    gamePlay.removeChild(input);
+}
+// creates the question and the input box when any of the controls is pressed
+const startQuestion =(event)=>{
+	proplem = question();
+	input = createAnswerInput();
+	gamePlay.appendChild(proplem);
+	gamePlay.appendChild(input);
+}
+
+
 
 positionFood();
-buttonUp.addEventListener("click",moveUp);
-buttonLeft.addEventListener("click",moveLeft);
-buttonDown.addEventListener("click",moveDown);
-buttonRight.addEventListener("click",moveRight);
+buttonUp.addEventListener("click",startQuestion);
+buttonLeft.addEventListener("click",startQuestion);
+buttonDown.addEventListener("click",startQuestion);
+buttonRight.addEventListener("click",startQuestion);
+
 
 
 
