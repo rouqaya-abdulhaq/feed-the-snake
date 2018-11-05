e726c7e5552a8fa3094bba44bc252c9f150ea06f
Rouqaya Abdulhaq <ruqia334@gmail.com>
2018-11-05 15:19:40 +0300

    added the question feature when pressing a control

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ let positionX = 0 ;
 let positionY = 0;
 let operators = ["+","-","*"];
 let gamePlay = document.querySelector("#game-play");
-let proplem;
-let input;
+let proplem = document.querySelector("#question");
+let input = document.querySelector("#answer");
 
 const positionFood =()=>{
     foodPosX = Math.random()*playfield.offsetWidth;
@@ -19,77 +19,58 @@ const positionFood =()=>{
     food.style.left = foodPosX + "px";
     food.style.top = foodPosY + "px";
 }
+//creates a random mathmatical proplem _[+,-,*] only_. 
+const randomProplem =()=>{
+	let num1 = Math.floor(Math.random()*256);
+    let num2 = Math.floor(Math.random()*256);
+    let operator = operators[Math.floor(Math.random()*operators.length)];
+    let proplem = num1+operator+num2;
+    return proplem; 
+}
+const validate =(proplem)=>{
+	if(event.keyCode===13){
+		if(input.value.length>0 &&!isNaN(input.value)){
+			return(eval(proplem)==input.value);
+		}}
+}
+const startQuestion =()=>{
+	let question = randomProplem();
+	proplem.innerText = question;
+}
+const getAnswer =()=>{
+	input.addEventListener("keypress",()=>{validate(proplem.innerText);});
+}
 const moveLeft =()=>{
+	startQuestion();
 	if(positionX>0){
 	positionX-=1;
-	snake.style.left = positionX + "em";} 
+	snake.style.left = positionX + "em";}
 }
 const moveRight =()=> {
-    if(positionX<playfield.offsetWidth){ 
-	positionX += 1 ;
-	snake.style.left = positionX +"em";}
+	startQuestion();
+	    if(positionX<playfield.offsetWidth){ 
+		positionX += 1 ;
+		snake.style.left = positionX +"em";}
 }
 const moveUp =()=>{
+	startQuestion();
 	if(positionY>0){
 	positionY -= 1;
 	snake.style.top = positionY + "em";} 
 }
 const moveDown = ()=>{
+	startQuestion();
 	if(positionY<playfield.offsetHeight){
 	positionY += 1;
 	snake.style.top = positionY + "em";}
 }
-const randomProplem =()=>{
-	let num1 = Math.floor(Math.random()*256);
-    let num2 = Math.floor(Math.random()*256);
-    let operator = operators[Math.floor(Math.random()*operators.length)];
-    let proplem = num1+operator+num2+" =";
-    return proplem; 
-}
-//returns a  simple random mathmatical proplem in string format 
-const question = ()=>{
-	let mathQuestion = document.createElement("p"); 
-	mathQuestion.appendChild(document.createTextNode(randomProplem()));
-	mathQuestion.id = "question";
-	return mathQuestion;
-}
-//returns an input box for the user input
-const createAnswerInput =()=>{
-	let userAnswer = document.createElement("input");
-	userAnswer.id = "answer";
-	return userAnswer;
-}
-//this method is not working properly
-//it should get the user answer and check if it is correct
-const validateAnswer =(event)=>{
-    if (input.value.length > 0 && event.keyCode === 13) {
-		return input.value === eval(proplem);
-	}
-}
-//this method should remove an existing question and input box
-//in case of correct answer or the user pressed the controls again
-const removeQuestion = (proplem,input)=>{
-    if (gamePlay.contains(proplem) && gamePlay.contains(input)){
-    	gamePlay.removeChild(proplem);
-    	gamePlay.removeChild(input);
-    }
-}
-// creates the question and the input box when any of the controls is pressed
-const startQuestion =()=>{
-	removeQuestion(proplem,input);
-	proplem = question();
-	input = createAnswerInput();
-	gamePlay.appendChild(proplem);
-	gamePlay.appendChild(input);
-}
-
 
 
 positionFood();
-buttonUp.addEventListener("click",startQuestion);
-buttonLeft.addEventListener("click",startQuestion);
-buttonDown.addEventListener("click",startQuestion);
-buttonRight.addEventListener("click",startQuestion);
+buttonUp.addEventListener("click",moveUp);
+buttonLeft.addEventListener("click",moveLeft);
+buttonDown.addEventListener("click",moveDown);
+buttonRight.addEventListener("click",moveRight);
 
 
 
